d4b540368ea2cccbb7fb447e0a4c64f4734cadc5
Untitled <104209781+kingaser@users.noreply.github.com>
2023-01-21 02:36:16 +0900

    [level 0] Title: 영어가 싫어요, Time: 0.89 ms, Memory: 72.3 MB -BaekjoonHub

--- 프로그래머스/lv0/120894. 영어가 싫어요/영어가 싫어요.java
+++ 프로그래머스/lv0/120894. 영어가 싫어요/영어가 싫어요.java
@@ -1,15 +1,6 @@
 class Solution {
-    public long solution(String n) {
-        n = n.replaceAll("zero", "0");
-        n = n.replaceAll("one", "1");
-        n = n.replaceAll("two", "2");
-        n = n.replaceAll("three", "3");
-        n = n.replaceAll("four", "4");
-        n = n.replaceAll("five", "5");
-        n = n.replaceAll("six", "6");
-        n = n.replaceAll("seven", "7");
-        n = n.replaceAll("eight", "8");
-        n = n.replaceAll("nine", "9");
-        return Long.valueOf(n);
+    public long solution(String numbers) {
+        long answer = 0;
+        return answer;
     }
 }
