932d61e37c29fcbad381b8046649c05051dedc9f
Untitled <104209781+kingaser@users.noreply.github.com>
2023-01-26 21:28:41 +0900

    [level 0] Title: 다항식 더하기, Time: 1.99 ms, Memory: 74.2 MB -BaekjoonHub

--- 프로그래머스/lv0/120863. 다항식 더하기/다항식 더하기.java
+++ 프로그래머스/lv0/120863. 다항식 더하기/다항식 더하기.java
@@ -1,19 +1,12 @@
 class Solution {
     public String solution(String polynomial) {
-        String answer = "";
-        int f = 0, s = 0;
-        for (String str : polynomial.split(" ")) {
-            if (str.contains("x")) f += str.equals("x") ? 1 : Integer.valueOf(str.substring(0, str.length()-1));
-            else if (!(str.equals("+"))) s += Integer.valueOf(str);
+        int xCnt = 0;
+        int cnt = 0;
+        for (String s : polynomial.split(" ")) {
+            if(s.contains("x")) xCnt += s.equals("x") ? 1 : Integer.parseInt(s.substring(0, s.length()-1));
+            else if (!(s.equals("+"))) cnt += Integer.parseInt(s);
         }
-        
-        String fStr = f > 0 ? f == 1 ? "x" : f + "x" : "";
-        String sStr = s > 0 ? String.valueOf(s) : "";
-        
-        if (f > 0) {
-            if (s > 0) answer += fStr + " + " + sStr;
-            else answer += fStr;
-        } else if(s > 0) answer += sStr;
-        return answer;
+        return (xCnt > 0 ? xCnt == 1 ? "x" : xCnt + "x" : "") + 
+            (cnt > 0 ? xCnt > 0 ? " + " + String.valueOf(cnt) : String.valueOf(cnt) : "");
     }
 }
