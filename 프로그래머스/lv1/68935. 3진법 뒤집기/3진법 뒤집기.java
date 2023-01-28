4bff751b382ea7bfd8b1af16f6c65198b76bce56
Untitled <104209781+kingaser@users.noreply.github.com>
2023-01-28 12:46:27 +0900

    [level 1] Title: 3진법 뒤집기, Time: 11.78 ms, Memory: 77.7 MB -BaekjoonHub

--- 프로그래머스/lv1/68935. 3진법 뒤집기/3진법 뒤집기.java
+++ 프로그래머스/lv1/68935. 3진법 뒤집기/3진법 뒤집기.java
@@ -1,25 +1,13 @@
-import java.util.ArrayList;
-import java.util.List;
-
 class Solution {
     public int solution(int n) {
         int answer = 0;
-        List<Integer> list = new ArrayList<>();
+        String a = "";
+        
         while (n > 0) {
-            list.add(n%3);
+            a += n%3;
             n /= 3;
         }
-        StringBuilder sb = new StringBuilder();
-        for (Integer x : list) {
-            sb.append(x);
-        }
-        for (int i = sb.length()-1, j = 0; i >= 0; i--, j++) {
-            if (i == sb.length()-1) answer = sb.charAt(i)-48;
-            else {
-                answer += (sb.charAt(i)-48)*(Math.pow(3, j));
-            }
-        }
-
-        return answer;
+        
+        return Integer.parseInt(a, 3);
     }
 }
