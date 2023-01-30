e5e948e75bd3358e127b8a48ec146619e0d6e91e
Untitled <104209781+kingaser@users.noreply.github.com>
2023-01-30 13:14:25 +0900

    [level 1] Title: 신규 아이디 추천, Time: 1.12 ms, Memory: 72.5 MB -BaekjoonHub

--- 프로그래머스/lv1/72410. 신규 아이디 추천/신규 아이디 추천.java
+++ 프로그래머스/lv1/72410. 신규 아이디 추천/신규 아이디 추천.java
@@ -1,55 +1,28 @@
 class Solution {
     public String solution(String new_id) {
-        // 대문자 소문자로 변환 및 문자열이 계속 변경되니 StringBuilder 사용
-        StringBuilder sb = new StringBuilder(new_id);
-        // 대문자 소문자로 변환
-        for (int i = 0; i < sb.length(); i++) {
-            if (Character.isUpperCase(sb.charAt(i))) {
-                sb.setCharAt(i, Character.toLowerCase(sb.charAt(i)));
-            }
+        // 1단계 new_id의 모든 대문자를 대응되는 소문자로 치환합니다.
+        String answer = new_id.toLowerCase();
+
+        // 2단계 new_id에서 알파벳 소문자, 숫자, 빼기(-), 밑줄(_), 마침표(.)를 제외한 모든 문자를 제거합니다.
+        answer = answer.replaceAll("[^-_.a-z0-9]","");
+        // 3단계 new_id에서 마침표(.)가 2번 이상 연속된 부분을 하나의 마침표(.)로 치환합니다.
+        answer = answer.replaceAll("[.]{2,}",".");
+        // 4단계 new_id에서 마침표(.)가 처음이나 끝에 위치한다면 제거합니다.
+        answer = answer.replaceAll("^[.]|[.]$","");
+        // 5단계 new_id가 빈 문자열이라면, new_id에 "a"를 대입합니다.
+        if(answer.equals(""))
+            answer+="a";
+        // 6단계 new_id의 길이가 16자 이상이면, new_id의 첫 15개의 문자를 제외한 나머지 문자들을 모두 제거합니다.
+        // 만약 제거 후 마침표(.)가 new_id의 끝에 위치한다면 끝에 위치한 마침표(.) 문자를 제거합니다.
+        if(answer.length() >=16){
+            answer = answer.substring(0,15);
+            answer=answer.replaceAll("^[.]|[.]$","");
         }
-        // 정규식 사용을 위해 String으로 변환
-        new_id = sb.toString();
-        // 정규식으로 2단계 조건 클리어
-        new_id = new_id.replaceAll("[^-|_.0-9a-z]", "");
-        // 3단계 마침표(.)가 2번 이상 연속된 부분 하나의 마침표로 치환 클리어
-        for (int i = 0; i < new_id.length(); i++) {
-            new_id = new_id.replaceAll("[.]+", ".");
-        }
-        // 다시 StringBuilder로 변환
-        sb = new StringBuilder(new_id);
-        // 5단계 문자열이 빈 문자열이면 a 대입 먼저 4단계 앞뒤에 마침표가 있으면 제거 클리어
-        sb = remove(sb);
-        // 6단계 아이디의 길이가 15가 넘으면 15이상의 문자는 제거 클리어
-        sb = sb.length() > 15 ? new StringBuilder(sb.substring(0, 15)) : sb;
-        sb = remove(sb);
-        // repeat을 사용하기 위해 String으로 변환
-        new_id = sb.toString();
-        // 아이디 길이가 3보다 작으면
-        if (new_id.length() < 3) {
-            // 아이디 길이가 1이면
-            if (new_id.length() == 1) {
-                // 문자열 자체를 3곱함
-                new_id = new_id.repeat(3);
-            } else {
-                // 아이디 길이가 2이면 두번째 문자 추가
-                new_id += new_id.charAt(1);
-            }
-        }
-        return new_id;
-    }
-    public static StringBuilder remove(StringBuilder str) {
-        StringBuilder sb = new StringBuilder(str);
-        while (true) {
-            if (sb.length() == 1 && sb.charAt(0) == '.') {
-                sb.append('a');
-            }
-            if (sb.charAt(0) == '.') {
-                sb.deleteCharAt(0);
-            } else if (sb.charAt(sb.length()-1) == '.') {
-                sb.deleteCharAt(sb.length()-1);
-            } else break;
-        }
-        return sb;
+        // 7단계 new_id의 길이가 2자 이하라면, new_id의 마지막 문자를 new_id의 길이가 3이 될 때까지 반복해서 끝에 붙입니다.
+        if(answer.length()<=2)
+            while(answer.length()<3)
+                answer+=answer.charAt(answer.length()-1);
+
+        return answer;
     }
 }
