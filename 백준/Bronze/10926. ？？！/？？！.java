63ffcfc3e346e709c4ccdca4e13f3218a210af34
Untitled <104209781+kingaser@users.noreply.github.com>
2023-06-08 11:35:37 +0900

    [Bronze V] Title: ??!, Time: 212 ms, Memory: 17732 KB -BaekjoonHub

---
+++ 백준/Bronze/10926. ？？！/？？！.java
@@ -0,0 +1,9 @@
+import java.util.*;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner kb = new Scanner(System.in);
+        String tmp = kb.nextLine();
+        System.out.println(tmp + "??!");
+    }
+}
