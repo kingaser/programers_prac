96ad907b0f5413732fd70cf39acedbecfbe9c942
Untitled <104209781+kingaser@users.noreply.github.com>
2023-07-02 16:22:16 +0900

    [Gold IV] Title: 카드 정렬하기, Time: 400 ms, Memory: 26484 KB -BaekjoonHub

--- 백준/Gold/1715. 카드 정렬하기/카드 정렬하기.java
+++ 백준/Gold/1715. 카드 정렬하기/카드 정렬하기.java
@@ -1,36 +1,40 @@
-import java.util.PriorityQueue;
-import java.util.Scanner;
+import java.io.*;
+import java.util.*;
 
-public class Main {
-	/**
-	 * 백준 1714 카드 정렬하기 (https://www.acmicpc.net/problem/1715)
-	 */
-	public static void main(String[] args) {
-		
-		//A+B번 비교
-		//고르는 순서에 따라 비교 횟수가 달라짐
-		//N max 100000
-		
-		Scanner sc = new Scanner(System.in);
-		int n = sc.nextInt();
-		
-		PriorityQueue<Long> pq = new PriorityQueue<Long>();
-		
-		for (int i=0; i<n; i++) {
-			pq.add(sc.nextLong());
-		}
-		
-		long num = 0;
-		//우선순위 큐에 2개이상 들어있어야 비교가 가능하다.
-		while (pq.size() > 1) {
-			long temp1 = pq.poll();
-			long temp2 = pq.poll();
-			
-			num += temp1 + temp2;
-			pq.add(temp1 + temp2); //합한 묶음 다시 넣기
-		}
-		
-		System.out.println(num);
-		
-	}
+class Main {
+    /*
+    우선순위 큐 사용 이유
+    앞에서 더한 값이 큐의 다음 값보다 크면 뒤로 미루기 위해서 사용
+    */
+    public String solution(long n, PriorityQueue<Long> pq) {
+        long sum = 0;
+        // 큐에서 2개의 값을 추출해서 더해야 하기 때문에 큐의 크기는 1보다 커야함
+        while (pq.size() > 1) {
+            long sum1 = pq.poll();
+            long sum2 = pq.poll();
+
+            // 변수 sum 에 누적합을 해줌
+            sum += sum1 + sum2;
+            // sum1과 sum2의 합을 큐에 다시 추가
+            pq.offer(sum1 + sum2);
+        }
+
+        return sum + "";
+    }
+
+    public static void main(String[] args) throws IOException {
+        Main T = new Main();
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        long n = Long.parseLong(br.readLine());
+        PriorityQueue<Long> pq = new PriorityQueue<>();
+        while (n-- > 0) {
+            pq.offer(Long.parseLong(br.readLine()));
+        }
+
+        bw.write(T.solution(n, pq));
+        bw.flush();
+        bw.close();
+    }
 }
