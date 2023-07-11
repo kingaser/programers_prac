5f567c69529eeb5d186f51e8403be09d153d8120
Untitled <104209781+kingaser@users.noreply.github.com>
2023-07-11 22:37:12 +0900

    Update 알고리즘 수업 － 삽입 정렬 3.py 다른 풀이 추가

--- 백준/Gold/24053. 알고리즘 수업 － 삽입 정렬 3/알고리즘 수업 － 삽입 정렬 3.py
+++ 백준/Gold/24053. 알고리즘 수업 － 삽입 정렬 3/알고리즘 수업 － 삽입 정렬 3.py
@@ -1,17 +1,41 @@
 def insert_sort(arrA, arrB):
+    s = 0
+    for i in range(n):
+        if arrA[i] == arrB[i]:
+            s += 1
+
     for i in range(1, len(arrA)):
         idx = i
         tmp = arrA[i]
         while idx > 0 and arrA[idx - 1] > tmp:
+            f = arrA[idx] == arrB[idx]
             arrA[idx] = arrA[idx - 1]
-            idx -= 1
-            if arrA == arrB:
+            if f and arrA[idx] != arrB[idx]:
+                s -= 1
+            elif not f and arrA[idx] == arrB[idx]:
+                s += 1
+            if s == n:
                 return 1
-        if arrA[idx] != tmp:
+            idx -= 1
+        if idx + 1 != i:
+            f = arrA[idx] == arrB[idx]
             arrA[idx] = tmp
-            if arrA == arrB:
+            if f and arrA[idx] != arrB[idx]:
+                s -= 1
+            elif not f and arrA[idx] == arrB[idx]:
+                s += 1
+            if s == n:
                 return 1
     return 0
+    #         arrA[idx] = arrA[idx - 1]
+    #         idx -= 1
+    #         if arrA == arrB:
+    #             return 1
+    #     if arrA[idx] != tmp:
+    #         arrA[idx] = tmp
+    #         if arrA == arrB:
+    #             return 1
+    # return 0
 
 
 
