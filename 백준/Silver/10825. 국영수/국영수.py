359b602d49bf3ba1706a4714aed661e6bb4fb909
Untitled <104209781+kingaser@users.noreply.github.com>
2023-07-20 18:42:11 +0900

    [Silver IV] Title: 국영수, Time: 584 ms, Memory: 156076 KB -BaekjoonHub

--- 백준/Silver/10825. 국영수/국영수.py
+++ 백준/Silver/10825. 국영수/국영수.py
@@ -6,4 +6,5 @@ score = [list(sys.stdin.readline().split()) for _ in range(n)]
 score.sort(key = lambda x: (-int(x[1]), int(x[2]), -int(x[3]), x[0]))
 
 for x in score:
-    sys.stdout.write(x[0] + "\n")
+    print(x[0])
+
