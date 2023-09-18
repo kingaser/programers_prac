b0fe093109de6eefcc0120227ff6d383a507192d
Untitled <104209781+kingaser@users.noreply.github.com>
2023-09-18 15:34:59 +0900

    [Silver II] Title: 예산, Time: 120 ms, Memory: 114328 KB -BaekjoonHub

---
+++ 백준/Silver/2512. 예산/예산.py
@@ -0,0 +1,26 @@
+import sys
+
+def solution(m, arr):
+    st, ed = 0, max(arr)
+    while st <= ed:
+        mid = (st + ed) // 2
+        tot = 0
+        for i in arr:
+            if i > mid:
+                tot += mid
+            else:
+                tot += i
+        if tot <= m:
+            st = mid + 1
+        else:
+            ed = mid - 1
+    return ed
+
+
+if __name__ == "__main__":
+    n = int(input())
+    arr = list(map(int, sys.stdin.readline().rstrip().split()))
+    m = int(input())
+
+    print(solution(m, arr))
+
