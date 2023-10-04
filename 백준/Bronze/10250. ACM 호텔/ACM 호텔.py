cc6ac0102d7ec26f323c12b1d5f5d69d7361428a
Untitled <104209781+kingaser@users.noreply.github.com>
2023-10-04 10:43:51 +0900

    [Bronze III] Title: ACM 호텔, Time: 84 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Bronze/10250. ACM 호텔/ACM 호텔.py
+++ 백준/Bronze/10250. ACM 호텔/ACM 호텔.py
@@ -1,18 +1,9 @@
-import sys
-
-def solution(h, n):
-    num = (n % h) * 100
-    floor = n // h + 1
-    if n % h == 0:
-        num = h * 100
-        floor = n // h
-    floor = num + floor
-    return floor
-
-
-if __name__ == "__main__":
-    n = int(input())
-
-    for _ in range(n):
-        h, w, n = map(int, sys.stdin.readline().rstrip().split())
-        print(solution(h, n))
+t = int(input())
+for _ in range(t):
+    h, w, n = map(int, input().split())
+    num = n//h+1
+    floor = n%h
+    if n%h == 0:
+        num = n//h
+        floor = h
+    print(f'{floor*100+num}')
