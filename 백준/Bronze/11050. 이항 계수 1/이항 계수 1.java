e553f8edd5e2dffee22a226fd21bfbc57644415b
Untitled <104209781+kingaser@users.noreply.github.com>
2024-02-29 23:06:41 +0900

    [Bronze I] Title: 이항 계수 1, Time: 128 ms, Memory: 14324 KB -BaekjoonHub

--- 백준/Bronze/11050. 이항 계수 1/이항 계수 1.java
+++ 백준/Bronze/11050. 이항 계수 1/이항 계수 1.java
@@ -17,7 +17,7 @@ class Main {
         }
 
         for (int i = 2; i <= n; i++) {
-            for (int j = 1; j <= n; j++) {
+            for (int j = 1; j <= i; j++) {
                 board[i][j] = board[i - 1][j - 1] + board[i - 1][j];
             }
         }
