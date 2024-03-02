f3cb16d873c387aa8bbd2f7ff5fc9d1055f6e3b2
Untitled <104209781+kingaser@users.noreply.github.com>
2024-03-02 13:48:42 +0900

    [Silver IV] Title: 링, Time: 124 ms, Memory: 14168 KB -BaekjoonHub

---
+++ 백준/Silver/3036. 링/링.java
@@ -0,0 +1,33 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
+
+        int n = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        for (int i = 1; i < n; i++) {
+            int gcd = gcd(arr[0], arr[i]);
+            int a = arr[0] / gcd;
+            int b = arr[i] / gcd;
+            sb.append(a).append("/").append(b).append("\n");
+        }
+
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
+    }
+
+    private static int gcd(int a, int b) {
+        if (b == 0) return a;
+        return gcd(b, a % b);
+    }
+}
