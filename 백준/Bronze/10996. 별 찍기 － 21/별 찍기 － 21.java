db24ffd32d9664a9516fbe8b8a40d853da5f6897
Untitled <104209781+kingaser@users.noreply.github.com>
2024-03-04 21:38:06 +0900

    [Bronze II] Title: 별 찍기 - 21, Time: 384 ms, Memory: 20428 KB -BaekjoonHub

---
+++ 백준/Bronze/10996. 별 찍기 － 21/별 찍기 － 21.java
@@ -0,0 +1,20 @@
+import java.io.*;
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < n * 2; i++) {
+            for (int j = 0; j < n; j++) {
+                if ((i + j) % 2 == 0) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
